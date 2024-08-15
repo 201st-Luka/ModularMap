a0158651178ffebe71e569a4024039f0f1592d03
201st-Luka <81482503+201st-Luka@users.noreply.github.com>
2024-08-15 21:42:37 +0200

    moved luka.modularmap.renderer into luka.modularmap.map

--- src/client/java/luka/modularmap/map/renderer/SurfaceRenderer.java
+++ src/client/java/luka/modularmap/map/renderer/SurfaceRenderer.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package luka.modularmap.renderer;
+package luka.modularmap.map.renderer;
 
 import luka.modularmap.map.MapChunk;
 import luka.modularmap.map.MapController;
